0a7d6bc074bec5a16a5ca575e8f5dd97849188a3
remynz <miranda.rebelloe@gmail.com>
2025-06-15 14:42:31 -0300

    Commit inicial do projeto WheelGo (backend e app)

---
+++ backend/src/server.ts
@@ -0,0 +1,41 @@
+import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORTA = process.env.PORTA || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.json({ message: 'Servidor Funcionando'});
+});
+
+const mongoURI = process.env.MONGO_URI;
+
+const startServer = async () => {
+
+    if(!mongoURI) {
+        console.error('MONGO_URI nao ta no env doidao');
+        process.exit(1);
+    }
+
+    try {
+
+        await mongoose.connect(mongoURI)
+        console.log('conectou no mongol ✅✅✅✅')
+
+        app.listen(PORTA, () => {
+            console.log(`server ta rodando na porta ${PORTA}`);
+        });
+    } catch (error) {
+        console.error('deu red❌❌❌', error);
+        process.exit(1);
+    }
+};
+
+startServer();
