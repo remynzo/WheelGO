43ab2115eb797cf2e836408d5e2816fe0d24604e
remynz <miranda.rebelloe@gmail.com>
2025-06-15 21:43:08 -0300

    login feito

--- backend/src/controller/userController.ts
+++ backend/src/controller/userController.ts
@@ -2,6 +2,7 @@ import { Request, Response, RequestHandler } from 'express';
 import UserModel from '../model/userModel';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import userModel from '../model/userModel';
 
 export const createUser: RequestHandler = async (req, res) => {
     try {
@@ -57,5 +58,57 @@ export const createUser: RequestHandler = async (req, res) => {
         console.error('Erro ao criar usuário:', error);
         res.status(500).json({ message: 'Ocorreu um erro interno no servidor.' });
     }
+}
 
-};
+    interface LoginBody {
+        email: string;
+        senha: string;
+    }
+  
+
+  export const loginUser: RequestHandler = async (req: Request<{}, {}, LoginBody>, res: Response) => {
+    try {
+
+        const { email, senha } = req.body;
+
+        if (!email || !senha) {
+            res.status(400).json({ message: 'E-mail e senha são obrigatórios.' });
+            return;
+        }
+
+        const userEncontrado = await UserModel.findOne({ email }).select('+senha');
+
+        if (!userEncontrado) {
+            res.status(401).json({ message: 'Credencias invalidos. '});
+            return;
+        }
+
+        const senhaCorreta = await bcrypt.compare(senha, userEncontrado.senha);
+
+        if(!senhaCorreta) {
+            res.status(401).json({ message: 'Credencias invalidos. '});
+            return;
+          }
+
+          const jwtSecret = process.env.JWT_SECRET;
+
+          if (!jwtSecret) {
+            throw new Error('o segredo jwt nao foi definido no .env')
+          }
+
+          const payload = { id: userEncontrado._id };
+          
+          const token = jwt.sign(payload, jwtSecret, { expiresIn: '30d'});
+          
+          const {senha: _, ...dadosDoUser } = userEncontrado.toObject();
+
+        res.status(200).json({
+            message: 'Login realizado com sucesso',
+            user: dadosDoUser,
+            token: token,
+        }); 
+        } catch (error){
+            console.error('Erro ao fazer login', error);
+            res.status(500).json({ message: 'Ocorreu um erro interno no servidor.' });
+        }
+    };
--- backend/src/rotas/userRotas.ts
+++ backend/src/rotas/userRotas.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import { createUser } from '../controller/userController';
+import { createUser, loginUser } from '../controller/userController';
 
 const router = Router();
 
 router.post('/register', createUser);
+
+router.post('/login', loginUser);
 
 export default router;
