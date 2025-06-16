510813c3673c9a623eac393b4fb59cad53f1164d
remynz <miranda.rebelloe@gmail.com>
2025-06-15 21:48:59 -0300

    authMiddleware feito

---
+++ backend/src/middleware/authMiddleware.ts
@@ -0,0 +1,47 @@
+import { Request, Response, NextFunction, RequestHandler } from 'express';
+import jwt from 'jsonwebtoken';
+import UserModel from '../model/userModel'; 
+
+interface AuthenticatedRequest extends Request {
+  user?: any; 
+}
+
+
+
+export const protect: RequestHandler = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+  let token;
+
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    try {
+
+      token = req.headers.authorization.split(' ')[1];
+
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as { id: string };
+
+
+      req.user = await UserModel.findById(decoded.id).select('-senha');
+      
+
+      if (!req.user) {
+
+        res.status(401).json({ message: 'Não autorizado, usuário não encontrado.' });
+        return;
+      }
+
+      next();
+      return; 
+
+    } catch (error) {
+      console.error(error);
+      res.status(401).json({ message: 'Não autorizado, token falhou.' });
+      return;
+    }
+  }
+
+
+  if (!token) {
+    res.status(401).json({ message: 'Não autorizado, nenhum token fornecido.' });
+    return; 
+  }
+};
