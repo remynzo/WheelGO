e6632dac31c1ffaed0d8f4670f9108989553bc36
remynz <miranda.rebelloe@gmail.com>
2025-06-20 11:44:07 -0300

    Adicionando WheelGoApp como uma pasta normal do projeto

---
+++ WheelGoApp/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import AppNavigator from './src/navigation/AppNavigator';
+
+function App(): React.JSX.Element {
+  // Agora o App simplesmente renderiza o nosso sistema de navegação.
+  return <AppNavigator />;
+}
+
+export default App;
